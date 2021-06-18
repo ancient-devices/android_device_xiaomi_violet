ac62aa690e22fb20a40fab62467a6eab24e32668
Arian <arian.kulmer@web.de>
2021-06-18 18:41:53 +0200

    violet: power: Import powerhint configuration from Pixel 4A
    powerhint.json is checked out to https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r39/powerhint.json.
    
    Change-Id: Ib576cea82363865cefd5e5fa2e7c746c0b95c3a8

--- device.mk
+++ device.mk
@@ -341,7 +341,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
 PRODUCT_COPY_FILES += \
